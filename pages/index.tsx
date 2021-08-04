81e04be2572c8ecfc1630feeed58e5246b9f4d9e
SorinCristescu <sorin.t.cristescu@gmail.com>
2021-08-04 14:42:06 +0300

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,24 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
-const IndexPage = () => (
+const IndexPage = (props) => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
     <p>
       <Link href="/about">
         <a>About</a>
       </Link>
+      <Link href="/service">
+        <a>Service</a>
+      </Link>
     </p>
   </Layout>
 )
 
 export default IndexPage
+
+export function getStaticProps() {
+  // fetch
+
+  
+}
---
+++ pages/service.tsx
@@ -0,0 +1,9 @@
+function Service() {
+    return (
+        <div>
+            Service page
+        </div>
+    )
+}
+
+export default Service
